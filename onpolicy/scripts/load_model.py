9582edd8df5e62bfe6e74a5a5ac90caee253a90f
gaoqingyuan-remote <qingyuangao212@gmail.com>
2022-03-10 11:43:09 +0000

    updated load_model.py

--- onpolicy/scripts/load_model.py
+++ onpolicy/scripts/load_model.py
@@ -17,7 +17,7 @@ class Struct:
         self.__dict__.update(entries)
 
 
-model_config = dict(gain=0.010,
+model_config = dict(gain=0.01,
                     hidden_size=64,
                     use_orthogonal=True,
                     use_policy_active_masks=True,
@@ -38,10 +38,11 @@ env = create_environment("academy_counterattack_hard",
                          number_of_right_players_agent_controls=0,
                          representation='simple115v2',
                          rewards='scoring,checkpoints',
-                         # write_goal_dumps=True,
-                         # write_full_episode_dumps=True,
-                         # write_video=True,
-                         # logdir='./wandb'
+                         write_goal_dumps=True,
+                         write_full_episode_dumps=True,
+                         write_video=True,
+                         logdir='./wandb',
+                         render=True
                          )
 
 # copied from FootballEnv
@@ -66,18 +67,20 @@ actor.eval()  # set eval mode
 
 
 # print(rnn_states.shape)
-runs = 10
+runs = 1
 for run in range(runs):
 
     rnn_states = np.zeros((4, 1, 64), dtype=np.float32)
     masks = np.ones((4, 1), dtype=np.float32)
     obs = env.reset()
+    # img = env.unwrapped.observation()[0].keys()
     dones = False
-    while not np.any(dones):
+    while not dones:
         action, action_prob, rnn_states = actor(obs, rnn_states, masks, deterministic=True)
         action = np.array(action.to('cpu')).squeeze()
-        obs, dones, rewards, infos = env.step(action)
+        obs, rewards, dones, infos = env.step(action)
         print(dones)
+        print(infos)
         # env.render()
-        img = env.unwrapped.observation()[0]["frame"]
-        print(img)
+        # img = env.unwrapped.observation()[0].keys()
+        # print(img)
